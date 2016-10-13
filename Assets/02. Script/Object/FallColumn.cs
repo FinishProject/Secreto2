7febafacb7ec44a5155e6f3fcfefc029f6c72a17
LeeRang <asw77id@gmail.com>
2016-10-13 15:50:57 +0900

    레벨 디자인 테스트 씬 생성

---
+++ Assets/02. Script/Object/FallColumn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallColumn : MonoBehaviour {
+
+
+    private bool isActive = true;
+	
+	// Update is called once per frame
+	void Update () {
+        if (isActive)
+        {
+            transform.Translate(Vector3.right * 0.8f * Time.deltaTime);
+            transform.RotateAround(Vector3.right, -2f * Time.deltaTime);
+        }
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.CompareTag("Land"))
+        {
+            isActive = false;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
--- Assets/02. Script/Player/PlayerCtrl.cs
+++ Assets/02. Script/Player/PlayerCtrl.cs
@@ -80,12 +80,19 @@ public class PlayerCtrl : MonoBehaviour
         if (inputAxis < 0 && isFocusRight) { TurnPlayer(); }
         // 오른쪽 회전
         else if (inputAxis > 0 && !isFocusRight) { TurnPlayer(); }
+
+        if (controller.isGrounded)
+        {
+            anim.SetBool("Jump", false);
+            anim.SetBool("Dash", false);
+            anim.SetBool("Idle", false);
+            //anim.SetBool("JumpDown", false);
+        }
     }
 
     void Movement()
     {
         inputAxis = Input.GetAxis("Horizontal"); // 키 입력
-        anim.SetFloat("Velocity", controller.velocity.y);
 
         // 좌우 동시 입력을 막기위함
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow) ||
@@ -99,19 +106,14 @@ public class PlayerCtrl : MonoBehaviour
         if (controller.isGrounded)
         {
             curGravity = 50f;
-            anim.SetBool("Jump", false);
-            anim.SetBool("Dash", false);
-            anim.SetBool("Idle", false);
 
             //이동
             moveDir = Vector3.right * inputAxis;
-
             // 점프
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 StartBasicJump();
             }
-
             // 키 입력 시 달리기 애니메이션 재생
             if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) ||
                 Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
@@ -134,7 +136,11 @@ public class PlayerCtrl : MonoBehaviour
                 StartDashJump();
 
             if (controller.velocity.y <= -0.01)
+            {
                 curGravity = dropGravity;
+                anim.SetFloat("Velocity", controller.velocity.y);
+                //anim.SetBool("JumpDown", true);
+            }
         }
 
         moveDir.y -= curGravity * Time.deltaTime;
