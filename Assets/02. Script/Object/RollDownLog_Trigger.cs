5637064827b5fc97722698c553afa5be0c206579
MBDS <kbs99q@naver.com>
2016-10-16 19:15:00 +0900

    ㅇㅅㅇ
    ㅇㅅㅇ

---
+++ Assets/02. Script/Object/RollDownLog_Trigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollDownLog_Trigger : MonoBehaviour {
+    public GameObject log;
+
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            log.GetComponent<RollDownLog>().StartRollDownLog();
+        }
+    }
+}
