558ccb04eb819ad44a6d7f74f8391608f34ab8a5
MBDS <kbs99q@naver.com>
2016-11-08 13:31:13 +0900

    ㅇㅅㅇ
    ㅇㅅㅇ/

--- Assets/02. Script/Camera/ZoomArea.cs
+++ Assets/02. Script/Camera/ZoomArea.cs
@@ -1,6 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public struct ZoomState
+{
+    [System.NonSerialized]
+    public float areaX;                 // 줌 구역 시작 X좌표 위치
+    [System.NonSerialized]
+    public float areaSize;              // 줌 구역 길이
+    public float deep;                  // 줌 깊이
+    public float height;                // 줌 높이
+    public float startRangePercent;     // 줌인, 아웃 시작 비율
+}
+
 public class ZoomArea : MonoBehaviour {
     public ZoomState zoomState;
     void Start()
