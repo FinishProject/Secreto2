244e54387da7339be9638186a662226ff03e7363
LeeRang <asw77id@gmail.com>
2017-08-14 18:40:37 +0900

    코드 정리

---
+++ Assets/02. Script/Object/Aggressive/BaseSphere.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseSphere : MonoBehaviour {
+
+    // 구체의 상태
+    public enum ChaseState
+    {
+        IDLE,
+        CHARGE,
+        SHOT,
+    }
+
+    protected ChaseState state = ChaseState.IDLE;
+
+    public Transform[] stones; // 각 돌들의 위치 배열
+    protected Vector3[] originPos; // 기존 위치를 저장할 배열
+    protected Vector3[] fillPos; // 벌어질 위치를 저장할 배열
+    public Vector3[] setFillPositions;
+
+    protected Transform playerTr; // 플레이어의 위치
+    protected AudioSource audioSoruce;
+
+    // 스피드
+    public float shootingSpeed = 100.0f; // 공격 속도
+    public float[] blockSpeed; // 각 돌들의 움직임 속도
+    public float reloadTime = 5.0f; // 재장전 속도
+
+    public float shakeAmount = 0.1f; // 흔들릴 강도
+    protected float moveDir = 1.0f;
+    protected bool isShot = false; // 공격 여부
+
+    protected virtual void Start()
+    {
+        audioSoruce = GetComponent<AudioSource>();
+        playerTr = PlayerCtrl.instance.transform;
+    }
+
+    /* 각 돌들의 초기 위치 저장 */
+    protected virtual Vector3[] SetBlockOriginPosition(Transform[] blockOriginPos)
+    {
+        Vector3[] originPosition = new Vector3[blockOriginPos.Length];
+
+        for (int i = 0; i < originPosition.Length; ++i)
+        {
+            originPosition[i] = blockOriginPos[i].position;
+        }
+
+        return originPosition;
+    }
+
+    protected virtual Vector3[] SetBlockFillPosition(Transform[] stonesPos, Vector3[] blockFillPos)
+    {
+        Vector3[] setFillPos = new Vector3[blockFillPos.Length];
+         
+        for(int i = 0; i < setFillPos.Length; ++i)
+        {
+            setFillPos[i] = new Vector3(stones[i].position.x + blockFillPos[i].x,
+                stones[i].position.y + blockFillPos[i].y, stones[i].position.z + blockFillPos[i].z);
+        }
+
+        return setFillPos;
+    }
+
+    protected virtual IEnumerator UpdateSphere()
+    {
+        // 상속받은 자식 클래스에서 정의
+        yield return null;
+    }
+
+    protected virtual IEnumerator Idle()
+    {
+        // 상속받은 자식 클래스에서 정의
+        yield return null;
+    }
+
+    protected virtual IEnumerator Charge()
+    {
+        // 상속받은 자식 클래스에서 정의
+        yield return null;
+    }
+
+    protected virtual IEnumerator Fire()
+    {
+        // 상속받은 자식 클래스에서 정의
+        yield return null;
+    }
+
+    protected virtual IEnumerator Fade(float fadeDir, float alpha)
+    {
+        // 상속받은 자식 클래스에서 정의
+        yield return null;
+    }
+}
---
+++ Assets/02. Script/Object/Aggressive/FireSphere.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireSphere : BaseSphere {
+
+    public GameObject fireBall;
+    public ShotFireball shotFire;
+
+    public Renderer render;
+    public ParticleSystem[] particle;
+    private Color[] color;
+
+    protected override void Start()
+    {
+        base.Start();
+        color = new Color[2];
+
+        // 초기 위치 저장
+        originPos = new Vector3[stones.Length];
+        if (originPos.Length > 0)
+        {
+            originPos = SetBlockOriginPosition(stones);
+        }
+
+        // 각 블록들이 벌어질 위치 설정
+        fillPos = new Vector3[setFillPositions.Length];
+        if (fillPos.Length > 0)
+        {
+            fillPos = SetBlockFillPosition(stones, setFillPositions);
+        }
+
+        StartCoroutine(UpdateSphere());
+    }
+    
+    /* 각 상태에 따른 코루틴 함수들을 업데이트 */
+    protected override IEnumerator UpdateSphere()
+    {
+        while (true)
+        {
+            switch (state)
+            {
+                case ChaseState.IDLE:
+                    yield return Idle();
+                    break;
+                case ChaseState.CHARGE:
+                    yield return Charge();
+                    break;
+                case ChaseState.SHOT:
+                    yield return ShotMove();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    /* 대기 상태, 각 돌들이 서로 각기의 움직임을 나타내도록 함 */
+    protected override IEnumerator Idle()
+    {
+        while (state == ChaseState.IDLE)
+        {
+            float moveSpeed = Mathf.Sin(Time.time);
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                float speed = moveSpeed * blockSpeed[i];
+                speed *= 0.1f;
+                stones[i].Translate(Vector3.right * (speed * moveDir) * Time.deltaTime);
+                moveDir *= -1f;
+            }
+            moveDir = 1f;
+
+            yield return null;
+        }
+    }
+
+    /* 충전 상태, 공격 모션 전의 각 돌들의 움직임을 나타냄. 
+     * 돌들이 임의의 방향으로 흔들림 */
+    protected override IEnumerator Charge()
+    {
+        while (state == ChaseState.CHARGE)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                stones[i].position = Vector3.Lerp(stones[i].position,
+                    fillPos[i], 2f * Time.deltaTime);
+                Vector3 oriPos = stones[i].localPosition;
+                stones[i].localPosition = oriPos + Random.insideUnitSphere * shakeAmount;
+            }
+
+            yield return null;
+        }
+    }
+
+    /* 발사 후 상태, 발사체를 발사한 후의 상태.
+     * 각 돌들의 벌어질 위치로 이동되어짐. */
+    IEnumerator ShotMove()
+    {
+        while (state == ChaseState.SHOT)
+        {
+            for (int i = 0; i < stones.Length; i++)
+            {
+                stones[i].position = Vector3.Lerp(stones[i].position, originPos[i], 7f * Time.deltaTime);
+            }
+            yield return null;
+        }
+    }
+
+    /* 발사 상태, 발사체 생성 후 플레이어를 향해 발사체 발사 */
+    protected override IEnumerator Fire()
+    {
+        while (true)
+        {
+            if (state == ChaseState.IDLE && !fireBall.activeSelf)
+            {
+                // 발사체 충전
+                state = ChaseState.CHARGE;
+                audioSoruce.Play();
+                yield return Fade(1, 0); // 발사체의 투명도를 조절함(Alpha : 1)
+
+                // 발사체 발사
+                state = ChaseState.SHOT;
+                audioSoruce.Stop();
+                Vector3 targetPos = new Vector3(playerTr.position.x, playerTr.position.y + 0.3f, playerTr.position.z);
+                shotFire.GetTarget(targetPos);
+                yield return new WaitForSeconds(0.5f);
+
+                // 대기 상태
+                state = ChaseState.IDLE;
+                yield return new WaitForSeconds(reloadTime);
+            }
+
+            // 플레이어가 빠져나간 후
+            if (!isShot)
+            {
+                fireBall.gameObject.SetActive(false);
+                break;
+            }
+            yield return null;
+        }
+    }
+
+    /* 발사체의 대한 투명도 조절 */
+    protected override IEnumerator Fade(float fadeDir, float alpha)
+    {
+        color[0] = render.material.color;
+        color[1] = particle[0].startColor;
+
+        for (int i = 0; i < particle.Length; i++)
+        {
+            color[1].a = alpha;
+            particle[i].startColor = color[1];
+        }
+
+        // 발사체를 현재 위치로 생성
+        fireBall.gameObject.SetActive(true);
+        fireBall.transform.position = transform.position;
+
+        // 알파값 조절
+        while (true)
+        {
+            alpha += fadeDir * 0.8f * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+            color[0].a = alpha;
+            color[1].a = alpha;
+
+            render.material.color = color[0];
+
+            if (alpha <= 0.6f)
+            {
+                for (int i = 0; i < particle.Length; i++)
+                    particle[i].startColor = color[1];
+            }
+
+            if (alpha == 1 || !isShot)
+                break;
+
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && !isShot)
+        {
+            isShot = true;
+
+            StartCoroutine(Fire());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player") && isShot)
+        {
+            isShot = false;
+        }
+    }
+}
